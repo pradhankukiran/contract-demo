066ba016db28ad72a84590215614839d3d531068
Kiran Kumar Pradhan <127883780+pradhankukiran@users.noreply.github.com>
2025-10-19 23:24:54 +0530

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import DraftContract from "./pages/DraftContract";
 import ReviewContract from "./pages/ReviewContract";
 import NotFound from "./pages/NotFound";
@@ -15,6 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <Analytics />
       <BrowserRouter>
         <Layout>
           <Routes>
